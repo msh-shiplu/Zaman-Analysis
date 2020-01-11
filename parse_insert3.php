5760b56bdaa15f7a496ba110aea60e006538e91c
Md Shiplu Hawlader <shiplu.cse.du@gmail.com>
2020-01-11 23:01:29 +0600

    added in main code

--- parse_insert3.php
+++ parse_insert3.php
@@ -9,68 +9,100 @@ function parse_company_detail($company_code){
 	$html = file_get_html('http://dsebd.org/company_details_nav.php?name='.$company_code);
 	$last = '';
 	$agm_flag = true;
-	foreach($html->find('font') as $a){
-		//$last = trim($last);
-		//echo $last.'<br/>';
-		 if(strstr($a->plaintext,'Last AGM Held:') && $agm_flag){
-			if(strlen($a->plaintext)>16)
-				$last_agm = trim(substr($a->plaintext,15,10));
-			else 
-				$last_agm='n/a';
-			$agm_flag = false;
+	$percantage = true;
+	$pecount1 = 0;
+	foreach($html->find("#company") as $a){
+		if ($agm_flag){
+				$st = strstr($a->plaintext, 'Last AGM held on:');
+				if ($st!==FALSE){
+						// if (strlen($st)>)
+						$st = trim(substr($st,18, 10));
+						if(strlen($st)==0)
+			$last_agm = 'n/a';
+		else
+			$last_agm=$st;
+						$agm_flag = false;
+						// print("AGM: ".$last_agm."\n");
+				}
+		}
+		foreach($a->find('tr td') as $x){
+			if (strstr($last, "Day's Range")!==FALSE){
+					$days_range = trim($x->plaintext);
+					// print("Days Range: ".$days_range."\n");
+			}
+			else if (strstr($last, "52 Weeks' Moving Range")!==FALSE){
+					$week_range = trim($x->plaintext);
+					// print("Week Range: ".$week_range."\n");
+			}
+			else if (strstr($last, "Total No. of Outstanding Securities")!==FALSE){
+					$total = intval(str_replace(',','',$x->plaintext));
+					// print("Total: ".$total."\n");
+			}
+			else if($last == 'Market Lot'){
+					sscanf($x->plaintext,"%d",$market_lot);
+					// print("market lot: ".$market_lot."\n");
+			}
+			else if (strstr($last, "Year End")!==FALSE){
+					$year_end = trim($x->plaintext);
+					// print("Year end: ".$year_end."\n");
+			}
+			else if(strstr($last,'Listing Year')){
+					sscanf($x->plaintext,"%d",$listing_year);
+					// print("listing year: ".$listing_year."\n");
+			}
+			else if(strstr($last,'Market Category')){
+					$category = trim($x->plaintext);
+					// print("Category: ".$category."\n");
+			}
+			else if(strstr($last, 'Current P/E Ratio using Basic EPS')){
+					$st = trim($x->parent()->last_child()->plaintext);
+					if ($st == '-')
+							$val = 0;
+					else
+							$val = (double)$st;
+					$pe1[] = $val;
+			}
+			else if(strstr($last, 'Current P/E ratio using Diluted EPS')){
+					$st = trim($x->parent()->last_child()->plaintext);
+					if ($st == '-')
+							$val = 0;
+					else
+							$val = (double)$st;
+					$pe2[] = $val;
+			}
+			else if($percantage && strstr($last,'Share Holding Percentage')){
+					foreach($x->find('table tr td') as $y){
+							$st = trim($y->plaintext);
+							if (strstr($st, 'Sponsor')){
+									// print("Sponsor: ".trim(substr($st, 18))."\n");
+									sscanf(substr($st, 18), "%f", $sponsor);
+							}
+							else if(strstr($st, 'Govt')){
+									// print("Govt: ".trim(substr($st, 6))."\n");
+									sscanf(substr($st, 6), "%f", $govt);
+							}
+							else if(strstr($st, "Institute")){
+									// print("Inst: ".trim(substr($st, 10))."\n");
+									sscanf(substr($st, 10), "%f", $institute);
+									$institute = (int)($institute * $total / 100);
+							}
+							else if(strstr($st, "Foreign")){
+									// print("Foreign: ".trim(substr($st, 8))."\n");
+									sscanf(substr($st, 8), "%f", $foreign);
+							}
+							else if(strstr($st, "Public")){
+									// print("Public: ".trim(substr($st, 8))."\n");
+									sscanf(substr($st, 8), "%f", $public);
+									$public = (int)($public * $total / 100);
+							}
+					}
+					// print($total." ".$public." ".$institute." ".$sponsor." ".$foreign." ".$govt."\n");
+					$percantage = false;
+			}
 			
-		 }
-		 else if($last == 'Year End&nbsp;'){
-			sscanf($a->plaintext,"%d",$year_end);
-		 }
-		 else if($last == 'Total no. of Securities'){
-			$total = intval(str_replace(',','',$a->plaintext));
-		 }
-		 else if(strstr($a->plaintext,'Public')){
-			sscanf($a->plaintext,"Public %f",$public_f);
-			$public = (int)($total*$public_f / 100.0);
-		 }
-		 else if(strstr($a->plaintext,'Institute')){
-			sscanf($a->plaintext,"Institute %f",$institute_f);
-			$institute = (int)($total*$institute_f / 100.0);
-		 }
-		 else if(strstr($a->plaintext,'Govt.')){
-			sscanf($a->plaintext,"Govt.%f",$govt);
-		 }
-		 else if(strstr($a->plaintext,'Sponsor')){
-			sscanf($a->plaintext,"Sponsor/Director %f",$sponsor);
-		 }
-		 else if(strstr($a->plaintext,'Foreign')){
-			sscanf($a->plaintext,"Foreign %f",$foreign);
-		 }
-		 else if($last == 'Market Lot'){
-			sscanf($a->plaintext,"%d",$market_lot);
-		 }
-		 else if($last == '52 Week\'s Range'){
-			$week_range = trim($a->plaintext);
-		 }
-		 else if(strstr($last,'Listing Year')){
-			sscanf($a->plaintext,"%d",$listing_year);
+			$last = $x->plaintext;
 		}
-		$last = $a->plaintext;
-		$a->clear();
-		unset($a);
-		
-	}
-	$last = '';
-	$html->clear();
-	$html = file_get_html('http://dsebd.org/company_details_nav.php?name='.$company_code);
-	foreach($html->find('table table table td') as $a){
-		if(strstr($last,'Market Category')){
-			sscanf($a->plaintext,"%s",$category);
-			$a->clear();
-			unset($a);
-			break;
-		}
-		$last = $a->plaintext;
-		$a->clear();
-		unset($a);
-	}
+    }
 	$html->clear();
 	unset($html);
 	unset($last);
@@ -85,12 +117,12 @@ function parse_company_detail($company_code){
 	//echo $query."\n";
 	$result = mysql_query($query);
 	if(mysql_fetch_array($result)){
-		$query = "update stock_data_detail set total=$total, public=$public, category='$category', year_end=$year_end, week_range='$week_range', institute=$institute, govt=$govt, sponsor=$sponsor, forgn=$foreign, market_lot=$market_lot, last_agm='$last_agm', listing_year=$listing_year where company_code='$company_code';";
+		$query = "update stock_data_detail set total=$total, public=$public, category='$category', year_end=$year_end, days_range='$days_range', week_range='$week_range', institute=$institute, govt=$govt, sponsor=$sponsor, forgn=$foreign, market_lot=$market_lot, last_agm='$last_agm', listing_year=$listing_year where company_code='$company_code';";
 		mysql_query($query);
 	//	echo $query."\n";
 	}
 	else{
-		$query = "insert into stock_data_detail values('$company_code',$total, $public, '$category', $year_end, '$week_range', $institute, $govt, $sponsor, $foreign, $market_lot, '$last_agm', $listing_year );";
+		$query = "insert into stock_data_detail values('$company_code',$total, $public, '$category', $year_end,'$days_range', '$week_range', $institute, $govt, $sponsor, $foreign, $market_lot, '$last_agm', $listing_year );";
 		mysql_query($query);
 	}
 	if(mysql_error()){
