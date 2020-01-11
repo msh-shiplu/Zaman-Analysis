ea0b478bbbc29e0788a7ccba481ba348ac87bfaa
Md Shiplu Hawlader <shiplu.cse.du@gmail.com>
2020-01-11 17:22:53 +0600

    updated

--- test.php
+++ test.php
@@ -1,53 +1,57 @@
 <?php
 include_once('/var/www/html/simple_html_dom.php');
-function do_your_job(){
-        $html = file_get_html('http://dsebd.org');
-        if(!$html){
-                unset($html);
-                exit(-1);
-        }
-        $last = '';
-        foreach($html->find('div div div div div div') as $a){
-                if(!$a->children(0)){
-                        $a->clear();
-                        unset($a);
-                        continue;
-                }
-                if(trim($a->children(0)->plaintext) == "DSEX Index"){
-                        //echo "DSEX Index: ".$a->children(1)->plaintext." ".$a->children(2)->plaintext."<br/>";
-                        sscanf($a->children(1)->plaintext,"%lf",$dsex1);
-                        sscanf($a->children(2)->plaintext,"%lf",$dsex2);
-                }
-                else if(trim($a->children(0)->plaintext) == "DSES Index"){
-                        //echo "DSES Index: ".$a->children(1)->plaintext." ".$a->children(2)->plaintext."<br/>";
-                        sscanf($a->children(1)->plaintext,"%lf",$dses1);
-                        sscanf($a->children(2)->plaintext,"%lf",$dses2);
-                }
-                else if(trim($a->children(0)->plaintext) == "DS30 Index"){
-                        //echo "DS30 Index: ".$a->children(1)->plaintext." ".$a->children(2)->plaintext."<br/>";
-                        sscanf($a->children(1)->plaintext,"%lf",$dse301);
-                        sscanf($a->children(2)->plaintext,"%lf",$dse302);
-                }
-                else if($last == "Total Trade"){
-                        sscanf($a->children(2)->plaintext,"%d",$total_value);
-                }
-                else if($last == "Issues Advanced"){
-                        sscanf($a->children(0)->plaintext,"%d",$issue_advanced);
-                        sscanf($a->children(1)->plaintext,"%d",$issue_declined);
-                        sscanf($a->children(2)->plaintext,"%d",$issue_unchanged);
-                        $a->clear();
-                        unset($a);
-                        break;
-                } 
-                $last = trim($a->children(0)->plaintext);
-                $a->clear();
-                unset($a);
-        }
-        $html->clear();
-        unset($html);
-        unset($last);
-	echo $dsex1.' '.$dsex2.' '.$dses1.' '.$dses2.' '.$dse301.' '.$dse302.' '.$total_value.' '.$issue_advanced.' '.$issue_declined.' '.$issue_unchanged.'\n';
+function parse_company_detail($company_code){
+	//echo $company_code."\n";
+	$html = file_get_html('http://dsebd.org/company_details_nav.php?name='.$company_code);
+	$last = '';
+	$agm_flag = true;
+	foreach($html->find("table[id='company'") as $a){
+		print($a->plaintext);
+	}
+	// $last = '';
+	// $html->clear();
+	// $html = file_get_html('http://dsebd.org/company_details_nav.php?name='.$company_code);
+	// foreach($html->find('table table table td') as $a){
+	// 	if(strstr($last,'Market Category')){
+	// 		sscanf($a->plaintext,"%s",$category);
+	// 		$a->clear();
+	// 		unset($a);
+	// 		break;
+	// 	}
+	// 	$last = $a->plaintext;
+	// 	$a->clear();
+	// 	unset($a);
+	// }
+	// $html->clear();
+	// unset($html);
+	// unset($last);
+	// $con = mysql_connect('localhost','root','Allahisalmighty');
+	// mysql_select_db('dsebd',$con);
+	// if(mysql_error()){
+	// 	echo "Fail to connect in $company_code \n";
+	// 	return;
+	// }
+	// //$company_code1 = str_replace(array("(",")","&","-","\\","/"),'',$company_code);
+	// $query = "select * from stock_data_detail where company_code='$company_code';";
+	// //echo $query."\n";
+	// $result = mysql_query($query);
+	// if(mysql_fetch_array($result)){
+	// 	$query = "update stock_data_detail set total=$total, public=$public, category='$category', year_end=$year_end, week_range='$week_range', institute=$institute, govt=$govt, sponsor=$sponsor, forgn=$foreign, market_lot=$market_lot, last_agm='$last_agm', listing_year=$listing_year where company_code='$company_code';";
+	// 	mysql_query($query);
+	// //	echo $query."\n";
+	// }
+	// else{
+	// 	$query = "insert into stock_data_detail values('$company_code',$total, $public, '$category', $year_end, '$week_range', $institute, $govt, $sponsor, $foreign, $market_lot, '$last_agm', $listing_year );";
+	// 	mysql_query($query);
+	// }
+	// if(mysql_error()){
+	// 	echo $query.' '.mysql_error()."\n";
+	// }
+	// mysql_close($con);
+	// unset($query);
+	// unset($result);
+	// unset($con);
 }
-do_your_job();
+parse_company_detail('ABBANK');
 ?>
 
